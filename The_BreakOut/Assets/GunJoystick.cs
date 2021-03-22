025a8ce33e083814812f274010233ea629a4052b
DropBear25 <michelle25@live.co.uk>
2021-03-22 17:07:10 +0000

    Controllers need work

--- The_BreakOut/Assets/GunJoystick.cs
+++ The_BreakOut/Assets/GunJoystick.cs
@@ -7,28 +7,35 @@ using System;
 public class GunJoystick : MonoBehaviour
 {
 
-    public Joystick joystick;
-    public GameObject Object;
-    Vector2 GameobjectRotation;
-    private float GameobjectRotation2;
-    private float GameobjectRotation3;
+	public Joystick joystick;
+	public GameObject Object;
+	Vector2 GameobjectRotation;
+	private float GameobjectRotation2;
+	private float GameobjectRotation3;
 
-    public bool FacingRight = true;
-
-
-    void Awake()
-    {
-     //   aimTransform = transform.Find("Aim");
-     //   aimAnimator = aimTransform.GetComponent<Animator>();
-    }
+	public bool FacingRight = true;
 
 
+	//[SerializeField] private Transform pbullet;
+
+	public GameObject bulletprefab;
+	public Transform firePoint;
+
+	float lookAngle;
+	public float bulletSpeed = 50;
+
+	[HideInInspector]
+	public bool canShoot = true;
+	private float nextTimeOfFire = 0;
+
+	
 
 
 
-    void Update()
-    {
-		HandleShooting();
+
+	void Update()
+	{
+		Shoot();
 
 		//Gets the input from the jostick
 		GameobjectRotation = new Vector2(joystick.Horizontal, joystick.Vertical);
@@ -36,11 +43,12 @@ public class GunJoystick : MonoBehaviour
 
 		if (FacingRight)
 		{
-			
+
 			//Rotates the object if the player is facing right
 			GameobjectRotation2 = GameobjectRotation.x + GameobjectRotation.y * 90;
 			Object.transform.rotation = Quaternion.Euler(0f, 0f, GameobjectRotation2);
-		//	HandleShooting();
+			//	firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
+			//	HandleShooting();
 		}
 		else
 		{
@@ -58,12 +66,10 @@ public class GunJoystick : MonoBehaviour
 			// Executes the void: Flip()
 			Flip();
 		}
+	
+	
 	}
 
-	//private void HandleShooting()
-	//{
-		//throw new NotImplementedException();
-	//}
 
 	private void Flip()
 	{
@@ -72,10 +78,18 @@ public class GunJoystick : MonoBehaviour
 
 		transform.Rotate(0, 180, 0);
 	}
-	private void HandleShooting()
+	private void Shoot()
 	{
-		
+		canShoot = false;
+		Instantiate(bulletprefab, firePoint.position, transform.rotation);
+		//	StartCoroutine(ShotCooldown());
+
 	}
+	//	IEnumerator ShotCooldown()
+	//{
+
+	//yield return new WaitForSeconds(timeBetweenShots);
+	//	canShoot = true;
 
 }
 
---
+++ The_BreakOut/Assets/VirtualJoystick.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+
+    private Image treshold;
+    private Image touch;
+
+    [HideInInspector]
+    public Vector3 InputDir;
+    [HideInInspector]
+    public bool shoot;
+
+
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 position = Vector2.zero;  
+        
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(treshold.rectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out position))
+        {
+            position.x = (position.x / treshold.rectTransform.sizeDelta.x);
+            position.y = (position.y / treshold.rectTransform.sizeDelta.y);
+
+
+            float x = (treshold.rectTransform.pivot.x == 1f) ? position.x * 2 + 1 : position.x * 2 - 1;
+            float y = (treshold.rectTransform.pivot.y == 1f) ? position.y * 2 + 1 : position.y * 2 - 1;
+
+            InputDir = new Vector3(x, y, 0);
+            InputDir = (InputDir.magnitude > 1) ? InputDir.normalized : InputDir;
+
+            touch.rectTransform.anchoredPosition = new Vector3(InputDir.x * (treshold.rectTransform.sizeDelta.x / 2.5f),
+               InputDir.y * (treshold.rectTransform.sizeDelta.y / 2.5f));
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       // OnDrag()
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        treshold = GetComponent<Image>();
+        touch = transform.GetChild(0).GetComponent<Image>();
+        InputDir = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
